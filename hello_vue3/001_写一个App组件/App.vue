1248b30c4dfcd6fb9f6217ed159f8a77657c368d
guohaolian <1040071899@qq.com>
2025-11-29 17:39:09 +0800

    01

--- hello_vue3/001_写一个App组件/App.vue
+++ hello_vue3/001_写一个App组件/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="app">
     <h1>你好啊！</h1>
-    <Person/>
   </div>
 </template>
 
 <script lang="ts">
-  import Person from './components/Person.vue'
-
   export default {
-    name:'App', //组件名
-    components:{Person} //注册组件
+    name:'App' //组件名
   }
 </script>
 
