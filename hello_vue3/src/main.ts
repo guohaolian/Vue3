ff18bc3a3e2a267eb435497709c7da1c21ea81ed
guohaolian <1040071899@qq.com>
2025-11-30 12:03:58 +0800

    04

--- hello_vue3/src/main.ts
+++ hello_vue3/src/main.ts
@@ -1,13 +1,11 @@
-// 引入createApp用于创建应用
 import {createApp} from 'vue'
-// 引入App根组件
 import App from './App.vue'
-// 引入路由器
-import router from './router'
+// 第一步：引入pinia
+import {createPinia} from 'pinia'
 
-// 创建一个应用
 const app = createApp(App)
-// 使用路由器
-app.use(router)
-// 挂载整个应用到app容器中
+// 第二步：创建pinia
+const pinia = createPinia()
+// 第三步：安装pinia
+app.use(pinia)
 app.mount('#app')
